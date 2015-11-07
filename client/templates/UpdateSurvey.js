83db20fe6e95c8faec2d95802f547c4e95bc95ce
deneo <adi.craciun96@gmail.com>
2015-11-07 12:56:55 +0200

    Some improvements on Survey Update

--- client/templates/UpdateSurvey.js
+++ client/templates/UpdateSurvey.js
@@ -9,24 +9,78 @@ if (Meteor.isClient) {
   });
 
   Template.addQuestion.events({
-    "click .freequestion": function(event) {
+    "click #freequestion": function(event) {
+      console.log("MERGE");
       Questions.insert({
         type: "freequestion",
         surveyID: this.surveyID
       });
     },
-    "click .answer10": function(event) {
+    "click #answer10": function(event) {
       Questions.insert({
         type: "answer10",
         surveyID: this.surveyID
       });
     },
-    "click .starrating": function(event) {
+    "click #starrating": function(event) {
       Questions.insert({
         type: "starrating",
         surveyID: this.surveyID
       });
     }
   });
+
+  Template.question.helpers({
+    "equal": function (a, b) {
+      return a == b;
+    }
+  });
+
+  Template.question.events({
+    "keyup [name=question]": function(event) {
+      var data = event.target.value;
+      Questions.update(this._id, {
+        $set: { question: data }
+      });
+    },
+    "click [name=delete]": function(event) {
+      Questions.remove(this._id);
+    }
+  });
+
+
+  Template.question.rendered = function() {
+   this.$('.editQuestion').sortable({
+       stop: function(e, ui) {
+         // get the dragged html element and the one before
+         //   and after it
+         el = ui.item.get(0)
+         before = ui.item.prev().get(0)
+         after = ui.item.next().get(0)
+
+         // Here is the part that blew my mind!
+         //  Blaze.getData takes as a parameter an html element
+         //    and will return the data context that was bound when
+         //    that html element was rendered!
+         if(!before) {
+           //if it was dragged into the first position grab the
+           // next element's data context and subtract one from the rank
+           newRank = Blaze.getData(after).rank - 1
+         } else if(!after) {
+           //if it was dragged into the last position grab the
+           //  previous element's data context and add one to the rank
+           newRank = Blaze.getData(before).rank + 1
+         }
+         else
+           //else take the average of the two ranks of the previous
+           // and next elements
+           newRank = (Blaze.getData(after).rank +
+                      Blaze.getData(before).rank)/2
+
+         //update the dragged Item's rank
+         Questions.update({_id: Blaze.getData(el)._id}, {$set: {rank: newRank}})
+       }
+   })
+ }
 
 }
