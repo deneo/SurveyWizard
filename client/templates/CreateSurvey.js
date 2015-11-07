9045a5c0d5023316f623f02f1782181bb0f68d45
deneo <adi.craciun96@gmail.com>
2015-11-08 00:40:16 +0200

    Last commit

--- client/templates/CreateSurvey.js
+++ client/templates/CreateSurvey.js
@@ -5,12 +5,14 @@ if (Meteor.isClient) {
 
       var name = event.target.name.value;
       var description = event.target.description.value;
+      var requireLogin = event.target.login.checked;
 
       var _id = Surveys.insert({
         name: name,
         description: description,
         createdAt: new Date(),
         owner: Meteor.userId(),
+        requireLogin: requireLogin,
         username: Meteor.user().username
       });
 
--- client/templates/ViewStatistics.js
+++ client/templates/ViewStatistics.js
@@ -4,4 +4,18 @@ if (Meteor.isClient) {
       return Questions.find({surveyID: this.surveyID}).fetch();
     }
   });
+  Template.questionStatistics.helpers({
+    "chart": function() {
+      if (this.type == "answer10" || this.type == "starrating") {
+        var list = Answers.find({questionID: this._id}).fetch();
+        var sum = 0;
+        for (var i = 0; i < list.length; i++)
+          sum += Number(list[i].answer);
+        return sum / list.length;
+      }
+    },
+    "equal": function (a, b) {
+      return a == b;
+    }
+  });
 }
